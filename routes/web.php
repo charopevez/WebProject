09736f9749c6ed1c1217a02cd5ebe13d7e931b3b
CosmosLexie <imayday_lexie@yahoo.co.jp>
2020-06-18 16:06:24 +0900

    added routes and blank files

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/search', function () {
+        return view('search');
+    });
+
+    Route::get('/details', function () {
+        return view('details');
+    });
+
+
+
