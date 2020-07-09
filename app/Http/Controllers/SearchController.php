c4bdf8df510172adb0049da9867197c526f7ada8
charopevez <charopevez@gmail.com>
2020-07-09 20:45:18 +0900

    queue test

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\UpdateID\UpdateIDJob;
+use App\Jobs\UpdateID\UpdateIDMainJob;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -9,6 +11,8 @@ class SearchController extends Controller
     //商品検索
     function search(Request $request){
     	$data=$request->search;
+    	$job=new UpdateIDMainJob();
+    	$this->dispatch($job);
         return view('/sresult', compact('data'));
     }
 }
