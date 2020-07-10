f0d65c1565567e553bf36aa5bf54e156fc557e74
Fai <2190180@ecccomp.com>
2020-07-10 16:40:28 +0900

    blade template test 2

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,14 +6,14 @@
   <title>52BANANA</title>
 
   <!-- Bootstrap Core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <!-- Custom Fonts -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://use.fontawesome.com/releases/v5.0.1/css/all.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
   <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
 
   <!-- Custom CSS -->
-  <link href="../css/stylish-portfolio.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/bootstrap-4.1.3.min.css">
-    <link rel="stylesheet" href="../css/style.css">
+  <link href="{{ asset('css/stylish-portfolio.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-4.1.3.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
