36116e9375cbba58485d80e8f291a8c3b92ba9b9
Fai <2190180@ecccomp.com>
2020-09-14 09:17:09 +0900

    added en mark

--- resources/views/includes/results.blade.php
+++ resources/views/includes/results.blade.php
@@ -2,7 +2,7 @@
 <div class="single-job mb-4 d-lg-flex justify-content-between">
 
     <div class="job-img align-self-center">
-        <img src="{{$itemDetails->ImgSRC}}" class="img-thumbnail" alt="{{$itemDetails->ItemName}}">
+        <img src="{{$itemDetails->ImgSRC}}" class="img-thumbnail" alt="{{$itemDetails->ItemName}}" style="width:60%">
     </div>
     
     <div class="job-text">
@@ -16,24 +16,24 @@
     <div class="job-btn align-self-center">
         @switch (request()->option)
                 @case ('Amazon')
-                @if(!empty($itemDetails->AmazonPrice))
-                <h4>{{$itemDetails->AmazonPrice}}</h4> 
-                <a href="{{$itemDetails->AmazonLink}}" class="third-btn job-btn2">Link</a>   
-                @endif
+                    @if(!empty($itemDetails->AmazonPrice))
+                        <h4>{{$itemDetails->AmazonPrice}}　円</h4> 
+                        <a href="{{$itemDetails->AmazonLink}}" class="third-btn job-btn2">Link</a>   
+                    @endif
                 @break
                   
                 @case ('Rakuten')
-                @if(!empty($itemDetails->RakutenPrice))
-                <h4>{{$itemDetails->RakutenPrice}}</h4>  
-                <a href="{{$itemDetails->RakutenLink}}" class="third-btn job-btn2">Link</a> 
-                @endif
+                    @if(!empty($itemDetails->RakutenPrice))
+                        <h4>{{$itemDetails->RakutenPrice}}　円</h4>  
+                        <a href="{{$itemDetails->RakutenLink}}" class="third-btn job-btn2">Link</a> 
+                    @endif
                 @break
 
                 @case ('Yahoo')
-                @if(!empty($itemDetails->YahooPrice))
-                <h4>{{$itemDetails->YahooPrice}}</h4>
-                <a href="{{$itemDetails->YahooLink}}" class="third-btn job-btn2">Link</a>
-                @endif
+                    @if(!empty($itemDetails->YahooPrice))
+                        <h4>{{$itemDetails->YahooPrice}}　円</h4>
+                        <a href="{{$itemDetails->YahooLink}}" class="third-btn job-btn2">Link</a>
+                    @endif
                  @break
 
                  @default
